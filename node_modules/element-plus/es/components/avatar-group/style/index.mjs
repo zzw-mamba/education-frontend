e65cd696949d06fd771cb85c6ac61f7c3801e075
FuNingYuan <23373125>
2026-03-26 20:51:25 +0800

    chore: 停止跟踪 node_modules

--- node_modules/element-plus/es/components/avatar-group/style/index.mjs
+++
@@ -1,4 +0,0 @@
-import '../../base/style/index.mjs';
-import 'element-plus/theme-chalk/src/avatar-group.scss';
-import '../../tooltip/style/index.mjs';
-//# sourceMappingURL=index.mjs.map
--- node_modules/element-plus/lib/components/avatar/src/constants.d.ts
+++
@@ -1,7 +0,0 @@
-import type { InjectionKey } from 'vue';
-import type { AvatarProps } from './avatar';
-export interface AvatarGroupContext {
-    size?: AvatarProps['size'];
-    shape?: AvatarProps['shape'];
-}
-export declare const avatarGroupContextKey: InjectionKey<AvatarGroupContext>;
