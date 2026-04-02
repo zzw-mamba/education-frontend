b0b5a4f7f4f6c7ff8e28786f16da177ff84867b6
zzw <22371437@buaa.edu.cn>
2026-04-02 23:39:46 +0800

    chore: stop tracking node_modules

--- node_modules/element-plus/es/components/avatar-group/style/css.mjs
+++
@@ -1,4 +0,0 @@
-import '../../base/style/css.mjs';
-import 'element-plus/theme-chalk/el-avatar-group.css';
-import '../../tooltip/style/css.mjs';
-//# sourceMappingURL=css.mjs.map
--- node_modules/element-plus/es/components/avatar-group/style/index.d.ts
+++
@@ -1,3 +0,0 @@
-import 'element-plus/es/components/base/style';
-import 'element-plus/theme-chalk/src/avatar-group.scss';
-import 'element-plus/es/components/tooltip/style';
